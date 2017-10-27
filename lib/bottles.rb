05506696ae628cf81161081115925b2bfb25abde
segunn <sarahgunnpostbox@gmail.com>
2017-10-27 21:36:56 +0100

    First test

---
+++ lib/bottles.rb
@@ -0,0 +1,8 @@
+class Bottles
+
+  def verse(i)
+    "#{i} bottles of beer on the wall, #{i} bottles of beer.\n" +
+    "Take one down and pass it around, #{i-1} bottles of beer on the wall.\n"
+  end
+
+end

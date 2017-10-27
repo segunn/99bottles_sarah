beca92278c9c15cf34f5057438bb7573d460a45d
segunn <sarahgunnpostbox@gmail.com>
2017-10-27 21:44:56 +0100

    5 specs pass

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,20 +1,32 @@
 class Bottles
 
   def verse(i)
-    "#{plural_bottles(i)} of beer on the wall, #{plural_bottles(i)} of beer.\n" +
-    "Take #{removable_bottles(i)} down and pass it around, #{plural_bottles(i-1)} of beer on the wall.\n"
+    "#{plural_bottles(i, capitalise: true)} of beer on the wall, #{plural_bottles(i)} of beer.\n" +
+    "#{remove_a_bottle(i)}\n"
   end
 
   private
 
-  def plural_bottles(i)
+  def plural_bottles(i, capitalise: false)
     case i
     when 1
       "1 bottle"
     when 0
-      "no more bottles"
+      if capitalise
+        "No more bottles"
+      else
+        "no more bottles"
+      end
     else
       "#{i} bottles"
+    end
+  end
+
+  def remove_a_bottle(i)
+    if i == 0
+      "Go to the store and buy some more, 99 bottles of beer on the wall."
+    else
+      "Take #{removable_bottles(i)} down and pass it around, #{plural_bottles(i-1)} of beer on the wall."
     end
   end
 
